464eb87bc719517782089b57d00b86c6dbc1d76d
idungstanley <sundaystanley56@gmail.com>
2023-12-15 16:35:13 +0100

    added code for travel journal

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "fasts", quantity: 23, packed: true },
+  { id: 3, description: "belts", quantity: 23, packed: true },
 ];
 export default function App() {
   return (
@@ -25,13 +25,31 @@ function Logo() {
 }
 
 function Form() {
+  const resetInput = () => {
+    setDescription('')
+  }
+  const resetQuantity = () => {
+    setQuantity('')
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem);
+    resetInput()
+    resetQuantity();
+  }
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>what do you need for your 😍 trip?</h3>
-      <select ></select>
-      <input type="text" placeholder='item...' />
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({length: 20}, (_, index) => index + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
+      </select>
+      <input type="text" placeholder='item...' value={description} onChange={(e) => setDescription(e.target.value)} />
       <button>Add</button>
-    </div>
+    </form>
   )
 }
 
@@ -39,7 +57,7 @@ function Form() {
 function PackingList() {
   return (
     <div className='list'>
-      <ul>{initialItems.map(item => <Item item={item} key={initialItems.id}/>)}</ul>
+      <ul>{initialItems.map(item => <Item item={item} key={item.id} />)}</ul>
     </div>
   )
 }
@@ -47,7 +65,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>{item.description} {item.quantity}</span>
+      <span style={item.packed ? { textDecoration: 'line-through'} : {}}> {item.quantity} {item.description} </span>
       <button>❌</button>
     </li>
   )
