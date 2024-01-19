d57c3ca83160216f398d48022f467dad1bc5d2c9
idungstanley <sundaystanley56@gmail.com>
2024-01-19 14:00:04 +0100

    added code for sorting of array

--- starter/src/App.js
+++ starter/src/App.js
@@ -66,11 +66,25 @@ function Form({onAddItems}) {
 }
 
 
-function PackingList({items, onDeleteItems, onToggleItem}) {
+function PackingList({ items, onDeleteItems, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description))
+  if (sortBy === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed))
   return (
     <div className='list'>
-      <ul>{items.map(item => <Item item={item} onDeleteItems={onDeleteItems} onToggleItem={onToggleItem} key={item.id} />)}</ul>
+      <ul>{sortedItems.map(item => <Item item={item} onDeleteItems={onDeleteItems} onToggleItem={onToggleItem} key={item.id} />)}</ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">sort by input order</option>
+          <option value="description">sort by description</option>
+          <option value="packed">sort by packed status</option>
+        </select>
+      </div>
     </div>
+
+    
   )
 }
 
