812b2065a8b94c2b297edcedff6389601b91e230
idungstanley <sundaystanley56@gmail.com>
2024-01-19 09:58:04 +0100

    added code for calculating derived state for statistics

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react'
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "belts", quantity: 23, packed: true },
-// ];
+
 export default function App() {
   const [items, setItems] = useState([]);
   function handleAddItem(item) {
@@ -22,7 +18,7 @@ export default function App() {
       <Logo />
       <Form onAddItems={handleAddItem}  />
       <PackingList items={items} onDeleteItems={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items}/>
     </div>
   )
 }
@@ -88,10 +84,15 @@ function Item({ item, onDeleteItems, onToggleItem }) {
   )
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (items.length === 0) return <p className='stats'><em>Start adding some items to your packing list</em></p>
+  const numItems = items.length;
+  const numPacked = items.filter((item)=> item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100)
+  console.log(percentage);
   return (
     <footer className='stats'>
-      <em>🤭 you have X items on your list, and you already packed X (X%)</em>
+      <em>{percentage === 100 ? `You got everything! Ready to go ✈` :`🤭 you have ${numItems} ${numItems === 1 ? "item" : "items"}  on your list, and you already packed ${numPacked} (${percentage}%)`}</em>
     </footer>
   )
 }
